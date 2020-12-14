14921e7354efd51b4fd283eb3140b2ac754a9b47
Dennis McLeod <dennis.mcleod92@gmail.com>
2020-12-13 22:55:38 -0500

    adding route to get data for one mentor

--- controllers/mentorController.js
+++ controllers/mentorController.js
@@ -2,3 +2,5 @@ const handlerFactory = require('./handlerFactory');
 const User = require('../models/userModel');
 
 exports.getAllMentors = handlerFactory.getAll(User);
+
+exports.getOneMentor = handlerFactory.getOne(User);
